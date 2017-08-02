624bd475a6e92756538bdc4ec263ae2c5011564e
martov1 <joacobrandan@gmail.com>
2017-08-02 19:25:49 -0300

    burger button works

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+
+/* burguer Header toggle */
+document.getElementById('mobile-nav__burgerbutton').onclick = function() {
+    console.log()
+    document.getElementById('mobile-nav__side-menu-contaner').classList.toggle('menu-hidden');
+}
+
+
